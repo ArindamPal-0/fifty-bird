91405b9d87cfb517167a8ebf7ee8143ad5378ce6
Arindam Pal <arindampal2000@gmail.com>
2022-09-07 19:45:54 +0530

    The State Machine Update

---
+++ bird08/main.lua
@@ -0,0 +1,133 @@
+-- virtual resolution handling library
+push = require 'push'
+
+-- class OOP class library
+Class = require 'class'
+
+-- bird class we've written
+require 'Bird'
+
+-- pipe class we've written
+require 'Pipe'
+
+-- class representing pair of pipes together
+require 'PipePair'
+
+-- all code related to game state and state machines
+require 'StateMachine'
+require 'state/BaseState'
+require 'state/PlayState'
+require 'state/TitleScreenState'
+
+
+-- physical screen dimensions
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+-- virtual resolution dimensions
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+-- background image and starting scroll location (X axis)
+local background = love.graphics.newImage('background.png')
+local backgroundScroll = 0
+
+-- ground iimage and starting scroll location (X axis)
+local ground = love.graphics.newImage('ground.png')
+local groundScroll = 0
+
+-- spped at which we should scroll our images, scaled by dt
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+-- point at which we should loop our background back to X 0
+local BAKCGROUND_LOOPING_POINT = 413
+
+-- scrolling variable to pause the game when we collide with a pipe
+local scrolling = true
+
+function love.load()
+    -- initialize our nearest-neighbour filter
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- seed the RNG
+    math.randomseed(os.time())
+
+    -- app window title
+    love.window.setTitle('Fifty Bird')
+
+    -- initialize our nice-looking retro text fonts
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    mediumFont = love.graphics.newFont('flappy.ttf', 14)
+    flappyFont = love.graphics.newFont('flappy.ttf', 28)
+    hugeFont = love.graphics.newFont('flappy.ttf', 56)
+    love.graphics.setFont(flappyFont)
+
+    -- initialize our virtual resolution
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true
+    })
+
+    -- initialize state machine with all the state-returning functions
+    gStateMachine = StateMachine {
+        ['title'] = function() return TitleScreenState() end,
+        ['play'] = function() return PlayState() end,
+    }
+    gStateMachine:change('title')
+
+    -- initialize input table
+    love.keyboard.keysPressed = {}
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.keypressed(key)
+
+    -- add to our table of keys pressed this frame
+    love.keyboard.keysPressed[key] = true
+
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+--[[
+    New function used to check our global input table for keys we activated during this frame, looked up their string value.
+]]
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
+    end
+end
+
+function love.update(dt)
+    -- update background and ground scroll offsets
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
+        % BAKCGROUND_LOOPING_POINT
+
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
+        % VIRTUAL_WIDTH
+
+    -- now, we just update the state machine, which defers to the right state
+    gStateMachine:update(dt)
+
+    -- reset input table
+    love.keyboard.keysPressed = {}
+end
+
+function love.draw()
+    push:start()
+
+    -- draw state machine between the background and ground, which defers render logic to the currently active state
+    love.graphics.draw(background, -backgroundScroll, 0)
+    gStateMachine:render()
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+
+    push:finish()
+end
